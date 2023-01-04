2efd57a3d6622824d01b2a806261640811337090
E-Serenity <61506935+e-serenity@users.noreply.github.com>
2023-01-04 12:05:17 +0100

    Working static Github pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const repo = 'solana-pay-starter'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  env: {
+    NEXT_PUBLIC_OWNER_PUBLIC_KEY: 'EnAgw24ycRekUCbbE4yH815QdSytNVZVCrxXrm1mahBC',
+  },
+}
